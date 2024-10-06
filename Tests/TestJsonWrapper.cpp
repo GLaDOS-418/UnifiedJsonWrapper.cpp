4dc3287689a96bb15a00a67cb5f1e1c314843a23
GLaDOS-418 <arnobbhanja@live.com>
2024-10-07 01:46:54 +0530

    add more tests for updated IJsonWrapper apis.

--- Tests/TestJsonWrapper.cpp
+++ Tests/TestJsonWrapper.cpp
@@ -1,20 +1,42 @@
+/************************************************************************************
+ * @file TestIJsonWrapper.cpp
+ * @brief This file contains test cases for `Wrappers::IJsonWrapper` interface.
+ ************************************************************************************/
+
 #include <gtest/gtest.h>
 
 #include "Exceptions/XJsonError.hpp"
 #include "Implementations/NlohmannJsonWrapper.hpp"
 
+/**
+ * @brief Typed test fixture class for `Wrapper::IJsonWrapper` interface implementations.
+ * @tparam TJsonWrapperImpl Will be used by gtest to substitute types from `TYPED_TEST_SUITE` initialization.
+ */
 template<typename TJsonWrapperImpl>
 class TestIJsonWrapper : public ::testing::Test
 {
 public:
+    /**
+     * @brief Json object to be tested.
+     */
     TJsonWrapperImpl _jsonWrapper;
+
+    /**
+     * @brief Inner object for _jsonWrapper to be used.
+     */
     std::unique_ptr<TJsonWrapperImpl> _innerObject{std::make_unique<TJsonWrapperImpl>()};
 };
 
+/*
+ * @brief Type list of all the implementations of `Wrappers::IJsonWrapper` that are to be tested.
+ */
 using TestTypes = ::testing::Types<Wrappers::NlohmannJsonWrapper>;
 
-// NOTE: trailing comma required to avoid unnecessary compiler warning
-// Ref: https://github.com/google/googletest/issues/2271#issuecomment-665742471
+/*
+ * @brief Initialize typed test suite for the type list.
+ * @note trailing comma required to avoid unnecessary compiler warning.
+ * Ref: https://github.com/google/googletest/issues/2271#issuecomment-665742471
+ */
 TYPED_TEST_SUITE(TestIJsonWrapper, TestTypes, );
 
 TYPED_TEST(TestIJsonWrapper, DefaultJson)
@@ -28,16 +50,16 @@ TYPED_TEST(TestIJsonWrapper, DefaultJson)
 TYPED_TEST(TestIJsonWrapper, EmptyJson)
 {
     Wrappers::IJsonWrapper& jsonWrapper = this->_jsonWrapper;
-    const std::string expectedJson = R"(null)";
+    const std::string expectedJson = R"({})";
 
-    EXPECT_EQ(jsonWrapper.ToString(), expectedJson);
+    EXPECT_EQ(jsonWrapper.GetEmptyObject()->ToString(), expectedJson);
 }
 
 TYPED_TEST(TestIJsonWrapper, IntegerValue)
 {
     Wrappers::IJsonWrapper& jsonWrapper = this->_jsonWrapper;
     const std::string key = "Integer";
-    constexpr int value = 10;
+    constexpr int64_t value = 10;
 
     jsonWrapper.SetInt(key, value);
 
@@ -45,6 +67,40 @@ TYPED_TEST(TestIJsonWrapper, IntegerValue)
 
     EXPECT_EQ(jsonWrapper.ToString(), expectedJson);
     EXPECT_EQ(jsonWrapper.GetInt(key), value);
+    EXPECT_TRUE(jsonWrapper.HasKey(key));
+}
+
+TYPED_TEST(TestIJsonWrapper, UnsignedValue)
+{
+    Wrappers::IJsonWrapper& jsonWrapper = this->_jsonWrapper;
+    const std::string key = "UnsignedInteger";
+
+    // this should underflow to uint64 max when type converted
+    constexpr int64_t value = -1;
+    constexpr uint64_t expectedValue = std::numeric_limits<uint64_t>::max();
+
+    jsonWrapper.SetUnsigned(key, value);
+
+    const std::string uintMaxStr = std::to_string(expectedValue);
+    const std::string expectedJson = R"({"UnsignedInteger":)" + uintMaxStr + R"(})";
+
+    EXPECT_EQ(jsonWrapper.ToString(), expectedJson);
+    EXPECT_EQ(jsonWrapper.GetUnsigned(key), expectedValue);
+    EXPECT_TRUE(jsonWrapper.HasKey(key));
+}
+
+TYPED_TEST(TestIJsonWrapper, DoubleValue)
+{
+    Wrappers::IJsonWrapper& jsonWrapper = this->_jsonWrapper;
+    const std::string key = "Double";
+    constexpr double value = 3.14;
+
+    jsonWrapper.SetDouble(key, value);
+
+    const std::string expectedJson = R"({"Double":3.14})";
+
+    EXPECT_EQ(jsonWrapper.ToString(), expectedJson);
+    EXPECT_DOUBLE_EQ(jsonWrapper.GetDouble(key), value);
     EXPECT_TRUE(jsonWrapper.HasKey(key));
 }
 
@@ -95,21 +151,6 @@ TYPED_TEST(TestIJsonWrapper, StringValue)
     EXPECT_TRUE(jsonWrapper.HasKey(key));
 }
 
-// TODO(glados): implement double api.
-TYPED_TEST(TestIJsonWrapper, DISABLED_DoubleValue)
-{
-    // Wrappers::IJsonWrapper& jsonWrapper = this->_jsonWrapper;
-
-    // const std::string key = "Double";
-    // constexpr double value = 3.14;
-    //
-    // const std::string expectedJson = R"({"Double":3.14})";
-
-    // EXPECT_EQ(jsonWrapper.ToString(), expectedJson);
-    // EXPECT_DOUBLE_EQ(jsonWrapper.GetDouble(key), value);
-    // EXPECT_TRUE(jsonWrapper.HasKey(key));
-}
-
 TYPED_TEST(TestIJsonWrapper, InnerObject)
 {
     Wrappers::IJsonWrapper& jsonWrapper = this->_jsonWrapper;
@@ -144,6 +185,24 @@ TYPED_TEST(TestIJsonWrapper, IntegerKeyAbsent)
     EXPECT_TRUE(false == jsonWrapper.HasKey(key));
 }
 
+TYPED_TEST(TestIJsonWrapper, UnsignedIntegerKeyAbsent)
+{
+    Wrappers::IJsonWrapper& jsonWrapper = this->_jsonWrapper;
+    const std::string key = "UnsignedIntegerKey";
+
+    EXPECT_THROW(jsonWrapper.GetUnsigned(key), Wrappers::XJsonError);
+    EXPECT_TRUE(false == jsonWrapper.HasKey(key));
+}
+
+TYPED_TEST(TestIJsonWrapper, DoubleKeyAbsent)
+{
+    Wrappers::IJsonWrapper& jsonWrapper = this->_jsonWrapper;
+    const std::string key = "DoubleKey";
+
+    EXPECT_THROW(jsonWrapper.GetDouble(key), Wrappers::XJsonError);
+    EXPECT_TRUE(false == jsonWrapper.HasKey(key));
+}
+
 TYPED_TEST(TestIJsonWrapper, BooleanKeyAbsent)
 {
     Wrappers::IJsonWrapper& jsonWrapper = this->_jsonWrapper;
@@ -151,16 +210,6 @@ TYPED_TEST(TestIJsonWrapper, BooleanKeyAbsent)
 
     EXPECT_THROW(jsonWrapper.GetBool(key), Wrappers::XJsonError);
     EXPECT_TRUE(false == jsonWrapper.HasKey(key));
-}
-
-// TODO(glados): implement double api
-TYPED_TEST(TestIJsonWrapper, DISABLED_DoubleKeyAbsent)
-{
-    // Wrappers::IJsonWrapper& jsonWrapper = this->_jsonWrapper;
-    // const std::string key = "DoubleKey";
-    //
-    // EXPECT_THROW(jsonWrapper.GetDouble(key), Wrappers::XJsonError);
-    // EXPECT_TRUE(false == jsonWrapper.HasKey(key));
 }
 
 TYPED_TEST(TestIJsonWrapper, StringKeyAbsent)
@@ -199,8 +248,8 @@ TYPED_TEST(TestIJsonWrapper, ParseWellFormedJson)
           }
         })";
 
-    //NOTE: 1) the keys get ordered.
-    //NOTE: 2) even the list (which is not supported yet, gets serialized).
+    // NOTE: 1) the keys get ordered.
+    // NOTE: 2) even the list (which is not supported yet, gets serialized).
     const std::string expectedJson = R"({"answer":{"everything":42},"happy":true,)"
                                      R"("list":[1,0,2],"name":"json","nothing":null,)"
                                      R"("object":{"currency":"USD","value":42.99},)"
@@ -210,4 +259,40 @@ TYPED_TEST(TestIJsonWrapper, ParseWellFormedJson)
     jsonWrapper.Parse(inputJson);
 
     EXPECT_EQ(jsonWrapper.ToString(), expectedJson);
+}
+
+TYPED_TEST(TestIJsonWrapper, ParseIllFormedJsonBadStructure)
+{
+    const std::string inputJson = R"(
+        {
+          "pi": 3.141,
+          "happy": true,
+          "name": "json",
+          "nothing": null,
+          "answer": {
+            "everything": 42
+          },
+          "list": [1, 0, 2],
+          "object": {
+            "currency": "USD",
+            "value": 42.99
+          }
+        )";  // notice the missing top-level closing brace.
+
+    Wrappers::IJsonWrapper& jsonWrapper = this->_jsonWrapper;
+
+    EXPECT_THROW(jsonWrapper.Parse(inputJson), Wrappers::XJsonError);
+}
+
+TYPED_TEST(TestIJsonWrapper, ParseIllFormedJsonNonUtf8)
+{
+    // Correct JSON structure, but the "message" contains invalid UTF-8 bytes
+    std::string invalidJson = R"({
+        "name": "John Doe",
+        "message": "\xC3\x28"
+    })";
+
+    Wrappers::IJsonWrapper& jsonWrapper = this->_jsonWrapper;
+
+    EXPECT_THROW(jsonWrapper.Parse(invalidJson), Wrappers::XJsonError);
 }
